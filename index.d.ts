b5d3c1fcb2bff55eca11961479c9a7c407728d5a
Vinicius Guedes <viniciusgued@gmail.com>
2018-03-02 18:19:25 -0300

    fix(CIScript): adds DB driver/adapter

--- index.d.ts
+++ index.d.ts
@@ -7,10 +7,17 @@ declare module "stackerjs-orm"
     export namespace ORM
     {
 
-        export class BaseRepository extends StackerJS.ORM.BaseRepository
-        { }
+        export abstract class BaseRepository extends StackerJS.ORM.BaseRepository
+        { 
+
+            abstract entity:StackerJS.ORM.IEntity;
+
+        }
 
         export class Util extends StackerJS.ORM.Util
+        { }
+
+        export interface IEntity extends StackerJS.ORM.IEntity
         { }
 
     }
