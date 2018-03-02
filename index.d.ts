6386aa4fe823dbe4520301625de299847c15892e
Vinicius Guedes <viniciusgued@gmail.com>
2018-03-02 18:16:53 -0300

    docs(types): adds types support

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,15 @@ import { StackerJS } from "stackerjs-types";
 declare module "stackerjs-orm"
 {
 
-    
+    export namespace ORM
+    {
+
+        export class BaseRepository extends StackerJS.ORM.BaseRepository
+        { }
+
+        export class Util extends StackerJS.ORM.Util
+        { }
+
+    }
 
 }
