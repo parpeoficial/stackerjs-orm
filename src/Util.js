a13f56be21555de5064cebbdf060d2d77ae47c0a
Vinicius Guedes <viniciusgued@gmail.com>
2018-04-03 11:41:48 -0300

    fix(validation): required integer field with 0 value

--- src/Util.js
+++ src/Util.js
@@ -1,66 +1,75 @@
-import { DB } from 'stackerjs-db';
+import { DB } from "stackerjs-db";
 
-
-export class Util
+export class Util 
 {
-
-    static makeEntity(defaultEntity, attributes)
+    static makeEntity(defaultEntity, attributes) 
     {
         let metadata = defaultEntity.metadata();
-        
-        let entity = Object.create(defaultEntity), _attributes = attributes;
-        metadata.fields.forEach(field => {
-            if (typeof attributes[field.name] !== 'undefined') {
+
+        let entity = Object.create(defaultEntity),
+            _attributes = attributes;
+        metadata.fields.forEach(field => 
+        {
+            if (typeof attributes[field.name] !== "undefined") 
+            {
                 let name = field.alias ? field.alias : field.name;
-                entity[name] = this.fieldValueParser(field.type, attributes[field.name]);
-                _attributes[field.name] = this.fieldValueParser(field.type, attributes[field.name])
+                entity[name] = this.fieldValueParser(
+                    field.type,
+                    attributes[field.name]
+                );
+                _attributes[field.name] = this.fieldValueParser(
+                    field.type,
+                    attributes[field.name]
+                );
             }
         });
 
-        Object.defineProperty(entity, '_attributes', {
-            'get': () => _attributes
+        Object.defineProperty(entity, "_attributes", {
+            get: () => _attributes
         });
 
-        metadata.relations
-            .forEach(relation => {
-                let relationGetterFunction = () => null;
-                if (relation.type === 'HASMANY')
-                    relationGetterFunction = this
-                        .HASMANYAssociation(entity, relation);
-                else if (relation.type === 'HASONE' || relation.type === 'BELONGSTO')
-                    relationGetterFunction = this
-                        .HASONEAssociation(entity, relation);
-                else if (relation.type === 'MANYMANY')
-                    relationGetterFunction = this
-                        .MANYMANYAssociation(entity, relation)
-
-                Object.defineProperty(entity, relation.name, {
-                    'get': relationGetterFunction
-                });
+        metadata.relations.forEach(relation => 
+        {
+            Object.defineProperty(entity, relation.name, {
+                get: () => 
+                {
+                    if (relation.type === "HASMANY")
+                        return this.HASMANYAssociation(entity, relation)();
+                    else if (
+                        relation.type === "HASONE" ||
+                        relation.type === "BELONGSTO"
+                    )
+                        return this.HASONEAssociation(entity, relation)();
+                    else if (relation.type === "MANYMANY")
+                        return this.MANYMANYAssociation(entity, relation)();
+                }
             });
+        });
 
         return Promise.resolve(entity);
     }
 
-    static fieldValueParser(type, value)
+    static fieldValueParser(type, value) 
     {
-        if (type === 'boolean')
-            return value === 1;
-        else if (type === 'date')
-            return new Date(value);
-        else if (type === 'json') {
-            try {
+        if (type === "boolean") return value === 1;
+        else if (type === "date") return new Date(value);
+        else if (type === "json") 
+        {
+            try 
+            {
                 return JSON.parse(value);
-            } catch (err) { 
-                return value; 
             }
-        } else if (type === 'created_at' || type === 'updated_at') 
+            catch (err) 
+            {
+                return value;
+            }
+        }
+        else if (type === "created_at" || type === "updated_at")
             return value ? value * 1000 : null;
-        else
-            return value;
+        else return value;
     }
 
-    static MANYMANYAssociation(entity, relation)
+    static MANYMANYAssociation(entity, relation) 
     {
         let expr = DB.Factory.getQueryCriteria();
         let queryBuilder = DB.Factory.getQueryBuilder()
@@ -68,53 +77,60 @@ export class Util
             .from(relation.table)
             .set(`${relation.referencedEntity.metadata().table}.*`)
             .join(
-                'INNER', 
-                relation.referencedEntity.metadata().table, 
+                "INNER",
+                relation.referencedEntity.metadata().table,
                 `${relation.table}.${relation.referencedField} = ` +
-                `${relation.referencedEntity.metadata().table}.id`
+                    `${relation.referencedEntity.metadata().table}.id`
             )
-            .where(expr.eq(relation.field, entity['_attributes']['id']));
+            .where(expr.eq(relation.field, entity["_attributes"]["id"]));
 
-        return () => queryBuilder.execute()
-            .then(results => Promise.all(
-                results.map(result => this
-                    .makeEntity(relation.referencedEntity, result))
-            ));
+        return () =>
+            queryBuilder
+                .execute()
+                .then(results =>
+                    Promise.all(results.map(result =>
+                        this.makeEntity(relation.referencedEntity, result))));
     }
 
-    static HASONEAssociation(entity, relation)
+    static HASONEAssociation(entity, relation) 
     {
         let expr = DB.Factory.getQueryCriteria();
         let queryBuilder = DB.Factory.getQueryBuilder()
             .select()
             .from(relation.referencedEntity.metadata().table)
-            .set('*')
-            .where(expr.eq(relation.referencedField, entity['_attributes'][relation.field]))
+            .set("*")
+            .where(expr.eq(
+                relation.referencedField,
+                entity["_attributes"][relation.field]
+            ))
             .limit(1);
 
-        return () => queryBuilder.execute()
-            .then(results => {
-                if (results.length <= 0)
-                    return Promise.resolve(null);
+        return () =>
+            queryBuilder.execute().then(results => 
+            {
+                if (results.length <= 0) return Promise.resolve(null);
 
                 return this.makeEntity(relation.referencedEntity, results[0]);
             });
     }
 
-    static HASMANYAssociation(entity, relation)
+    static HASMANYAssociation(entity, relation) 
     {
         let expr = DB.Factory.getQueryCriteria();
         let queryBuilder = DB.Factory.getQueryBuilder()
             .select()
             .from(relation.referencedEntity.metadata().table)
-            .set('*')
-            .where(expr.eq(relation.referencedField, entity['_attributes'][relation.field]));
+            .set("*")
+            .where(expr.eq(
+                relation.referencedField,
+                entity["_attributes"][relation.field]
+            ));
 
-        return () => queryBuilder.execute()
-            .then(results => Promise.all(
-                    results.map(result => this
-                        .makeEntity(relation.referencedEntity, result))
-                ));
+        return () =>
+            queryBuilder
+                .execute()
+                .then(results =>
+                    Promise.all(results.map(result =>
+                        this.makeEntity(relation.referencedEntity, result))));
     }
-
 }
