a25857daaae63f9c811d7d5c0f873ec7d3481761
Vinicius Guedes <viniciusgued@gmail.com>
2018-04-03 19:41:33 -0300

    improve(package): update dependencies

--- test/Unit/ORMErrors.test.js
+++ test/Unit/ORMErrors.test.js
@@ -1,73 +1,82 @@
-import { expect } from 'chai';
-import { DB } from './../../lib';
-import { ContactRepository } from './../DataProvider/Repositories';
+import { expect } from "chai";
+import { DB } from "stackerjs-db";
+import { ContactRepository } from "./../DataProvider/Repositories";
 
-
-describe('ORMErrorsTest', () => 
+describe("ORMErrorsTest", () => 
 {
-
-    describe('Testing some database problems', () => 
+    describe("Testing some database problems", () => 
     {
-        it('Should present database errors on inserting', done => 
+        it("Should present database errors on inserting", done => 
         {
             let contactRepository = new ContactRepository();
 
-            contactRepository.save({
-                'first_name': 'Allucard', 'last_name': 'Castlevania'
-            })
-                .then(response => {
+            contactRepository
+                .save({
+                    first_name: "Allucard",
+                    last_name: "Castlevania"
+                })
+                .then(response => 
+                {
                     expect(response).to.be.false;
-                    expect(contactRepository.getErrors()).to.have.property('Database');
+                    expect(contactRepository.getErrors()).to.have.property("Database");
                 })
                 .then(() => done());
         });
 
-        it('Should present database errors on updating', done => 
+        it("Should present database errors on updating", done => 
         {
             let contactRepository = new ContactRepository();
 
-            contactRepository.save({
-                '_attributes': {
-                    'id': 1
-                },
-                'id': 1,
-                'first_name': 'Allucard',
-                'last_name': 'Castlevania'
-            })
-                .then(response => {
-                    expect(response).to.be.false;
-                    expect(contactRepository.hasErrors()).to.be.true;
+            contactRepository
+                .save({
+                    _attributes: {
+                        id: 1
+                    },
+                    id: 1,
+                    first_name: "Allucard",
+                    last_name: "Castlevania"
                 })
-                .then(() => done());
-        });
-
-        it('Should present database errors on deleting', done =>
-        {
-            let contactRepository = new ContactRepository();
-
-            contactRepository.delete({
-                '_attributes': { 'id': 1 },
-                'id': 1,
-                'first_name': 'Allucard',
-                'last_name': 'CastleVania'
-            })
-                .then(response => {
+                .then(response => 
+                {
                     expect(response).to.be.false;
                     expect(contactRepository.hasErrors()).to.be.true;
                 })
                 .then(() => done());
         });
 
-        it('Should add Errors to repository', () => 
+        it("Should present database errors on deleting", done => 
         {
             let contactRepository = new ContactRepository();
 
-            contactRepository.addError(new Error('Errors added with Exception'));
-            contactRepository.addError('Adding another error to default field');
-            contactRepository.addError('Some field', 'Defining field before sending error');
+            contactRepository
+                .delete({
+                    _attributes: { id: 1 },
+                    id: 1,
+                    first_name: "Allucard",
+                    last_name: "CastleVania"
+                })
+                .then(response => 
+                {
+                    expect(response).to.be.false;
+                    expect(contactRepository.hasErrors()).to.be.true;
+                })
+                .then(() => done());
+        });
+
+        it("Should add Errors to repository", () => 
+        {
+            let contactRepository = new ContactRepository();
+
+            contactRepository.addError(new Error("Errors added with Exception"));
+            contactRepository.addError("Adding another error to default field");
+            contactRepository.addError(
+                "Some field",
+                "Defining field before sending error"
+            );
 
             expect(contactRepository.hasErrors()).to.be.true;
         });
     });
 
+    after(() => DB.Factory.getConnection().disconnect());
 });
