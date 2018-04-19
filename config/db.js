d0cc8ccd70e6061c83aed4ec05ede5fe917d0009
Vinicius Guedes <viniciusgued@gmail.com>
2018-04-19 20:37:24 -0300

    fix(package): update broken dependencies

---
+++ config/db.js
@@ -0,0 +1,9 @@
+import { Config } from "stackerjs-utils";
+
+module.exports = {
+    driver: Config.env("db.driver"),
+    host: Config.env("db.host"),
+    name: Config.env("db.name"),
+    user: Config.env("db.user"),
+    pass: Config.env("db.pass")
+};
