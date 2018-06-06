e4dc9a50f2f409cd993d62f72826d09294f9d2f5
Vinicius Guedes <viniciusgued@gmail.com>
2018-06-06 11:19:01 -0300

    refactor(BaseRepository): slice entity manipulator in new class

--- src/BaseRepository.js
+++ src/BaseRepository.js
@@ -1,9 +1,9 @@
 import { DB } from "stackerjs-db";
 import { Util } from "./Util";
-import { BaseRepositoryHooks } from "./BaseRepositoryHooks";
+import { BaseRepositoryEntities } from "./BaseRepositoryEntities";
 
 
-export class BaseRepository extends BaseRepositoryHooks 
+export class BaseRepository extends BaseRepositoryEntities 
 {
     constructor() 
     {
@@ -242,45 +242,4 @@ export class BaseRepository extends BaseRepositoryHooks
             });
     }
 
-    isNewRecord(entity) 
-    {
-        return (
-            !entity._attributes ||
-            typeof entity._attributes[this.getFieldByType("pk")] ===
-            "undefined" ||
-            !entity._attributes[this.getFieldByType("pk")]
-        );
-    }
-
-    getFieldByType(type, alias = false) 
-    {
-        for (let field of this.entity.metadata().fields) 
-        {
-            if (field.type === type)
-                return field.alias && alias ? field.alias : field.name;
-        }
-
-        return null;
-    }
-
-    prepare(entity) 
-    {
-        this.entity.metadata().fields.forEach(field => 
-        {
-            let fieldName = field.alias ? field.alias : field.name;
-            if (
-                typeof entity[fieldName] === "undefined" &&
-                (field.default || field.default === 0)
-            )
-                entity[fieldName] = field.default;
-        });
-    }
-
-    getCurrentTimeStamp() 
-    {
-        return parseInt(new Date()
-            .getTime()
-            .toString()
-            .slice(0, -3));
-    }
 }
